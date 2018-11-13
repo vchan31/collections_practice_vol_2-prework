0417c672f1d347ab8dbd07e82d277db4d9ff4630
sensible-proc-7557 <vchan313@gmail.com>
2018-11-13 21:25:15 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,64 @@
 # your code goes here
+def begins_with_r(tools)
+  counter = 0
+  tools.each do |element|
+    if element[0] == "r"
+      counter += 1
+        
+        end
+    end
+  if counter == tools.length
+  true
+  else 
+    false
+  end
+  end 
+  
+def contain_a(args)
+    args.map do |element|
+      element if element.include?("a")
+  end.compact
+end
+
+def first_wa(args)
+    args.each do |element|
+      return element if element[0] == "w" and element[1] == "a"
+      
+    end
+end
+
+
+
+def remove_non_strings(array1)
+  array1.collect do |element|
+    element if element.instance_of?(String) == true  
+  end.compact
+
+end
+
+def count_elements (array1)
+  list = []
+  answer = []
+  array1.each do |person|
+    person.each do |k,v|
+      list << v
+    end
+  end
+list.each do |element|
+    answer.push(:name => element, :count => list.count(element))
+  
+end
+answer.uniq
+end
+
+def merge_data(keys,data)
+  merged_hashes = []
+  data.each do |hash|
+    hash.each do |name, info|
+      keys.each do |key_val|
+        merged_hashes << key_val.merge(info) if key_val.values[0] == name
+      end
+    end
+  end
+  merged_hashes
+ end
